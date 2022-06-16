a38c1769d4c4278fcec4660ff48a498b3e43e1dd
“keyie” <570588655@qq.com>
2022-06-16 22:56:57 +0800

    feat: runtime update

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -11,13 +11,20 @@ function patch(vnode, container) {
   processComponent(vnode, container)
 }
 
-
 function processComponent(vnode: any, container: any) {
-  mountComponent(vnode)
+  mountComponent(vnode, container)
 }
 
-function mountComponent(vnode: any) {
+function mountComponent(vnode: any, container) {
   const instance = createComponentInstance(vnode)
 
   setupComponent(instance)
+
+  setupRenderEffect(instance, container)
+}
+
+function setupRenderEffect(instance, container) {
+  const subTree = instance.render()
+
+  patch(subTree, container)
 }
